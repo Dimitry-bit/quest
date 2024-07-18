cc1ed80cda35e23de3f37f85813cae6543478ba1
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2024-07-18 17:29:37 +0300

    feat: add custom searchbar component

---
+++ lib/components/quest_search_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class QuestSearchBar extends StatelessWidget {
+  final TextEditingController? controller;
+  final bool autoFocus;
+  final String? hintText;
+  final void Function(String?)? onSubmitted;
+
+  const QuestSearchBar({
+    super.key,
+    this.controller,
+    this.hintText,
+    this.onSubmitted,
+    this.autoFocus = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SearchBar(
+      controller: controller,
+      autoFocus: autoFocus,
+      hintText: hintText,
+      onSubmitted: onSubmitted,
+      elevation: const WidgetStatePropertyAll(0.0),
+      hintStyle: const WidgetStatePropertyAll(TextStyle(color: Colors.black45)),
+      leading: const Icon(Icons.search, size: 32.0, color: Colors.black54),
+    );
+  }
+}
